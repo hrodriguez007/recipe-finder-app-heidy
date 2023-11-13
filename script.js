45fefdc515d2b2b22f5eb34ecd097ca50409988e
hrodriguez007 <heidyrodriguez567@gmail.com>
2023-11-13 15:44:59 -0600

    Add files to the recipe-finder-app-heidy necessary

---
+++ script.js
@@ -0,0 +1,81 @@
+const searchBtn = document.getElementById('search-btn');
+const mealList = document.getElementById('meal');
+const mealDetailsContent = document.querySelector('.meal-details-content');
+const recipeCloseBtn = document.getElementById('recipe-close-btn');
+
+// event listeners
+//brings up the list of meals that have the searched ingredient
+searchBtn.addEventListener('click', getMealList);
+//brings up the actual recipe to that meal
+mealList.addEventListener('click', getMealRecipe);
+//exit out of recipe to video
+recipeCloseBtn.addEventListener('click', () => {
+    mealDetailsContent.parentElement.classList.remove('showRecipe');
+});
+
+
+//get a meal list that contains the searched ingredient
+function getMealList(){
+    let searchInputTxt = document.getElementById('search-input').value.trim();
+    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputTxt}`)
+    .then(response => response.json())
+    .then(data => {
+        let html = "";
+        if(data.meals){
+            data.meals.forEach(meal => {
+                html += `
+                    <div class="meal-item" data-id = "${meal.idMeal}">
+                        <div class = "meal-img">
+                            <img src="${meal.strMealThumb}" alt="photo of food">
+                        </div>
+                        <div class="meal-name">
+                            <h3>${meal.strMeal}</h3>
+                            <a href="#" class="recipe-btn">Get Recipe</a>
+                    </div>
+                    </div>
+                `;
+            });
+            mealList.classList.remove('notFound');
+        } else {
+            html = "Sorry, no meal was found with that ingredient!"
+            mealList.classList.add('notFound');
+        }
+
+        mealList.innerHTML = html;
+    });
+
+}
+
+
+//get the recipe to pull with the ingredient searched
+function getMealRecipe(e){
+    e.preventDefault();
+    if(e.target.classList.contains('recipe-btn')){
+        let mealItem = e.target.parentElement.parentElement;
+        fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.dataset.id}`)
+        .then(response => response.json())
+        .then(data => mealRecipeModal(data.meals));
+        }
+    }
+
+// create a modal and video link
+function mealRecipeModal(meal){
+    console.log(meal);
+    meal = meal[0];
+    let html = `
+        <h2 class = "recipe-title">${meal.strMeal}</h2>
+        <p class = "recipe-category">${meal.strCategory}</p>
+        <div class = "recipe-instruct">
+            <h3>Instructions:</h3>
+            <p>${meal.strInstructions}</p>
+        </div>
+        <div class = "recipe-meal-img">
+            <img src = "${meal.strMealThumb}" alt = "">
+        </div>
+        <div class = "recipe-link">
+            <a href = "${meal.strYoutube}" target = "_blank">Watch Video</a>
+        </div>
+    `;
+    mealDetailsContent.innerHTML = html;
+    mealDetailsContent.parentElement.classList.add('showRecipe');
+}
